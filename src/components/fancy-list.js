5e484446f488a18b5397483bdfc1e137672ffe26
mrjecekl <jonathanbogasky@gmail.com>
2023-12-23 13:04:52 -0600

    add files

---
+++ src/components/fancy-list.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { listBox, interactiveList } from './fancy-list.module.css'
+
+export function ChildList({ data }) {
+      let listItems = Object.entries(data).map(([key, value]) => {
+        let itemKey = `${data.name}.${key}`
+
+        if (key == 'name'){
+          return (
+            <li key={itemKey}>
+              <h3>
+                <a href={data.url} target='_blank' rel='noreferrer noopener'>{data.name}</a>
+              </h3>
+            </li>
+          );
+        }
+        else if (key !== 'name' && key !=='url') {
+          return <li key={itemKey}>{value}</li>;
+        }
+      });
+
+      return <ul>{listItems}</ul>;
+}
+
+export default function FancyList({ jsonData }) {
+  let fancyList = jsonData.content.map(data => {
+    return (
+      <li key={data.name} className={interactiveList}>
+        <ChildList data={data} />
+      </li>
+    )
+  });
+
+  return (
+    <div className={listBox}>
+      <h2>{jsonData.title}</h2>
+      <ul>{fancyList}</ul>
+    </div>
+  );
+}
---
+++ src/components/footer.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { footer, iconBox, linkIcon, textBox, copyrightBox } from './footer.module.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
+import { faCopyright } from '@fortawesome/free-regular-svg-icons'
+
+export default function Footer() {
+    return (
+        <div className={footer}>
+            <div className={iconBox}>
+                <a href="https://www.linkedin.com/in/jonathan-bogasky-530713b4" target='_blank' rel='noreferrer noopener'>
+                    <FontAwesomeIcon className={linkIcon} icon={faLinkedin}  />
+                </a>
+                <a href="https://github.com/mrjeckel" target='_blank' rel='noreferrer noopener'>
+                    <FontAwesomeIcon className={linkIcon} icon={faGithub} />
+                </a>
+            </div>
+            <div className={textBox}>
+                <span>jonathanbogasky@gmail.com</span>
+            </div>
+            <div className={copyrightBox}>
+                <FontAwesomeIcon className={linkIcon} icon={faCopyright} />
+                <span>2023 Jonathan Bogasky</span>
+            </div>
+        </div>
+    )
+}
